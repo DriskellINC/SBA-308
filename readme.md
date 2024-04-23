60b998103a044e4f0eb00433d1df29f9970f09c4
Dionte Driskell <driskell06@gmail.com>
2024-04-22 23:05:49 -0400

    final touches

---
+++ readme.md
@@ -0,0 +1,1 @@
+Using Javascript was able to create functions for desired results using try, catch, obj, arrays and formulas for learners submissions. Used Javascripts and HTML. 
--- script.js
+++ script.js
@@ -105,20 +105,37 @@ function getLearnerData(course, ag, submissions) {
 // try/catch -check to see if its 0 - if then - let and const appropriate usage - strings, numbers, booleans, value cached with variables 
 
 
-
-  CourseInfo (id, name) {
-  if (typeof id === "number" || typeof name === "string") {
-    throw new Error
-      ("Invalid input ID must be a number, name must be a string");
-  }
-} try {
-  CourseInfo(1, "");    //""is a sting in order to catch the error
-} catch (error) {
-  console.error (error.message);       //try and catch, booleen, sting and error caught 
-}                                       //'id'=number
+// /////////////////////////////////////////////////////////////
+// function CourseInfo (id, name) {
+//   if (typeof id === "number" || typeof name === "string") {
+//     throw new Error
+//       ("Invalid input ID must be a number, name must be a string");
+//   }
+// } try {
+//   CourseInfo(1, "");    //""is a sting in order to catch the error
+// } catch (error) {
+//   console.error (error.message);       //try and catch, booleen, sting and error caught 
+// }                                       //'id'=number
 
 /**   weighted average and assignment scores for each learner is needed:
 Iterate through the LearnerSubmission array and calculate the weighted average score for each learner.
 Handle late submissions by deducting 10% of the total points possible for assignments submitted after the due date.
 Calculate the percentage score for each assignment. */
 
+function calWeightedAverage(CourseInfo, AssignmentGroup, LearnerSubmissions) {
+  const learnerScores = {}; 
+  for ( const submission of LearnerSubmissions) {
+        const learnerId = submission.learner_id
+        const assignment_id = submission.assignment_id;
+        const submissionData = submission.submission;
+
+        const assignment = assignmentGroup
+          .find(assignment => assignment_id === assignmentId)
+        
+  }
+}
+const weightedScore = (submissionData.score / assignment.points_possible) * assignment.group_weight;
+
+// should go through assignments, submissions - points and possible points and weighted scores per assignment 
+
+// I was able to console.log some results, until i tired my try and catch from 109- then started to have problems. I have requested extra help via learner support!!!
